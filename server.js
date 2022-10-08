6deb910f990c7a9747fb90198590e43536e9f9ab
PabloGCH <pablochoconi@gmail.com>
2022-10-08 02:45:54 -0300

    Agregada carpeta public

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-//Imports
+//IMPORTS
 const express = require("express")
-//Global variables
+//GLOBAL VARIABLES
 const app = express()
 const productsRouter = require("./routes/products")
 
+//TOOLS
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use("/public", express.static("public"))
+
+//ROUTES
 app.use("/api/products", productsRouter);
 
+
+//SERVER INIT
 app.listen(4000, () => {
 	console.log("Server listening on port 4000")
 })
