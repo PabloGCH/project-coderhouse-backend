3a9c353d24abf411682d08dd7991e63ac5846bf1
PabloGCH <pablochoconi@gmail.com>
2022-10-08 02:34:13 -0300

    Ahora se usa ruteo de express

---
+++ routes/products.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const Container = require("../container.js")
+const container = new Container("./products.json")
+
+
+router.get("/", (req, res) => {
+	container.getAll().then(data => {
+		res.send(data);
+	})
+})
+
+router.get("/:id", (req, res) => {
+	let {id} = req.params;
+	container.getById(id).then(data => {
+		if(data) {
+			res.send(data);
+		} else {
+			res.send({error: "Producto no encontrado"})
+		}
+	})
+})
+
+
+router.post("/", (req, res) => {
+	container.save(req.body).then(data => {
+		res.send(data);
+	})
+})
+
+
+router.put("/:id", (req,res) => {
+	let {id} = req.params;
+	container.edit(req.body, id).then(data => {
+		if(data.success) {
+			res.send("Producto modificado");
+		} else {
+			res.send("Error al modificar producto");
+		}
+	})
+})
+
+
+router.delete("/:id", (req, res) => {
+	let {id} = req.params;
+	container.deleteById(id).then(data => {
+		if(data.success) {
+			res.send("Producto eliminado");
+		} else {
+			res.send("Error al eliminar archivo");
+		}
+	}) 
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,63 +1,14 @@
 //Imports
 const express = require("express")
-const Container = require("./container.js")
 //Global variables
-const container = new Container("./products.json")
 const app = express()
-
+const productsRouter = require("./routes/products")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.get("/api/products", (req, res) => {
-	container.getAll().then(data => {
-		res.send(data);
-	})
-})
-
-app.get("/api/products/:id", (req, res) => {
-	let {id} = req.params;
-	container.getById(id).then(data => {
-		if(data) {
-			res.send(data);
-		} else {
-			res.send({error: "Producto no encontrado"})
-		}
-	})
-})
-
-
-app.post("/api/products", (req, res) => {
-	container.save(req.body).then(data => {
-		res.send(data);
-	})
-})
-
-
-app.put("/api/products/:id", (req,res) => {
-	let {id} = req.params;
-	container.edit(req.body, id).then(data => {
-		if(data.success) {
-			res.send("Producto modificado");
-		} else {
-			res.send("Error al modificar producto");
-		}
-	})
-})
-
-
-app.delete("/api/products/:id", (req, res) => {
-	let {id} = req.params;
-	container.deleteById(id).then(data => {
-		if(data.success) {
-			res.send("Producto eliminado");
-		} else {
-			res.send("Error al eliminar archivo");
-		}
-	}) 
-})
-
+app.use("/api/products", productsRouter);
 
 app.listen(4000, () => {
 	console.log("Server listening on port 4000")
 })
+
